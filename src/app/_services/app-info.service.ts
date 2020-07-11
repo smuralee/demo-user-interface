e93eeb3db177ea4b18e383776dc91930e49ccf32
smuralee <suraj@surajmuraleedharan.com>
2020-07-11 14:19:48 +0100

    Updated the method

--- src/app/_services/app-info.service.ts
+++ src/app/_services/app-info.service.ts
@@ -1,27 +1,22 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {AppService} from './app-service';
-import {AppInfo} from '../_models';
-import {Router} from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppService } from './app-service';
+import { AppInfo } from '../_models';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppInfoService extends AppService {
-
-  readonly resourceName = '/login';
+  readonly resourceName = '/register';
 
   constructor(private http: HttpClient, private router: Router) {
     super();
   }
 
-  login(payload: AppInfo) {
-    this.http.post(this.getEndpoint(this.resourceName), payload)
-      .subscribe(data =>
-        this.router.navigate(['summary'])
-      )
-    ;
+  register(payload: AppInfo) {
+    this.http
+      .post(this.getEndpoint(this.resourceName), payload)
+      .subscribe((data) => this.router.navigate(['summary']));
   }
-
-
 }
--- src/app/app-info/app-info.component.ts
+++ src/app/app-info/app-info.component.ts
@@ -1,41 +1,50 @@
-import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {AppInfoService} from '../_services/app-info.service';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AppInfoService } from '../_services/app-info.service';
 
 @Component({
   selector: 'app-app-info',
   templateUrl: './app-info.component.html',
   styleUrls: ['./app-info.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppInfoComponent implements OnInit {
-
   appFormGroup: FormGroup;
 
-  constructor(private service: AppInfoService) {
-  }
+  constructor(private service: AppInfoService) {}
 
   ngOnInit(): void {
     this.appFormGroup = new FormGroup({
-      firstName: new FormControl('', [Validators.required, Validators.maxLength(60)]),
-      lastName: new FormControl('', [Validators.required, Validators.maxLength(60)]),
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(60),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(60),
+      ]),
       email: new FormControl('', [
         Validators.required,
         Validators.email,
-        Validators.pattern('[\\w-]+@([\\w-]+\\.)+[\\w-]+')
+        Validators.pattern('[\\w-]+@([\\w-]+\\.)+[\\w-]+'),
       ]),
       gender: new FormControl(''),
       dob: new FormControl(),
       address: new FormControl('', [Validators.required]),
-      notes: new FormControl('')
+      notes: new FormControl(''),
     });
   }
 
   onSubmit() {
     if (this.appFormGroup.valid) {
       console.log(JSON.stringify(this.appFormGroup.value, null, 4));
-      this.service.login(this.appFormGroup.value);
+      this.service.register(this.appFormGroup.value);
     }
   }
 
@@ -46,5 +55,4 @@ export class AppInfoComponent implements OnInit {
   hasError(controlName: string, errorName: string) {
     return this.appFormGroup.controls[controlName].hasError(errorName);
   }
-
 }
